909331b6ddbde7f846a41cc22eafc3008cfa148e
Brandon Martin <brandon.martin101@gmail.com>
2017-01-13 17:27:31 -0700

    initial setup

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT || 3000;
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+  res.render('index');
+});
+
+app.listen(port, function() {
+  console.log("Node app is running on port " + port);
+});
